6b2320f023628ab0cef28aa0bd7c5444b2fc30bb
Samuel <sam_olayemi65@yahoo.com>
2022-07-07 10:54:43 +0100

    Fixed signup template

--- core/forms.py
+++ core/forms.py
@@ -18,8 +18,12 @@ class CheckoutForm(forms.Form):
 		
 
 class CustomSignUpForm(SignupForm):
-	first_name = forms.CharField(max_length=30, label='First Name')
-	last_name = forms.CharField(max_length=30, label='Last Name')
+	first_name = forms.CharField(max_length=30, label='First Name', widget=forms.TextInput(attrs={
+			'placeholder':'First Name',
+		}))
+	last_name = forms.CharField(max_length=30, label='Last Name', widget=forms.TextInput(attrs={
+			'placeholder':'Last Name',
+		}))
 
 	def signup(self, request, user):
 		user.first_name = self.cleaned_data['first_name']
