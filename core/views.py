01d5e5b9efdad5d5164394f1e56d3ae42405d35e
Samuel <sam_olayemi65@yahoo.com>
2022-07-08 09:22:33 +0100

    Fixed Object does not exist in add to cart

--- core/views.py
+++ core/views.py
@@ -95,30 +95,33 @@ def add_to_cart(request, slug):
 			messages.info(request, "This item was added to your cart")
 			return redirect('cart')
 	else:
-		item = get_object_or_404(Item, slug=slug)
-		order_item = OrderItem.objects.get(
-			item=item,
-			user=request.user,
-			ordered=False,
-			)
-		order_qs = Order.objects.filter(user=request.user, ordered=False)
-		if order_qs.exists():
-			order = order_qs[0]
-			if order.items.filter(item__slug=item.slug).exists():
-				order_item.quantity += 1
-				order_item.save()
-				messages.info(request, "This item quantity was updated")
-				return redirect('cart')
+		try:
+			item = get_object_or_404(Item, slug=slug)
+			order_item = OrderItem.objects.get(
+				item=item,
+				user=request.user,
+				ordered=False,
+				)
+			order_qs = Order.objects.filter(user=request.user, ordered=False)
+			if order_qs.exists():
+				order = order_qs[0]
+				if order.items.filter(item__slug=item.slug).exists():
+					order_item.quantity += 1
+					order_item.save()
+					messages.info(request, "This item quantity was updated")
+					return redirect('cart')
+				else:
+					messages.info(request, "This item was added to your cart")
+					order.items.add(order_item)
+					return redirect('cart')
 			else:
-				messages.info(request, "This item was added to your cart")
+				ordered_date = timezone.now()
+				order = Order.objects.create(user=request.user, ordered_date=ordered_date)
 				order.items.add(order_item)
+				messages.info(request, "This item was added to your cart")
 				return redirect('cart')
-		else:
-			ordered_date = timezone.now()
-			order = Order.objects.create(user=request.user, ordered_date=ordered_date)
-			order.items.add(order_item)
-			messages.info(request, "This item was added to your cart")
-			return redirect('cart')
+		except ObjectDoesNotExist:
+				return redirect('cart')
 
 		
 
