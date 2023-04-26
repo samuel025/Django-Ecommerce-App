504ae8bc3de75f2b3875ec0535a45d0e3566205f
Samuel <sam_olayemi65@yahoo.com>
2023-04-26 16:55:40 +0100

    still fixing

--- store/settings.py
+++ store/settings.py
@@ -162,7 +162,7 @@ ACCOUNT_FORMS = {
     'signup': 'core.forms.CustomSignUpForm'
 }
 
-STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
